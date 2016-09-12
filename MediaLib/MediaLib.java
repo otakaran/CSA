5eb95beb8ae0311020fa24a20c34595f526249a5
Otakar Andrysek <otakar-sst@users.noreply.github.com>
2016-09-12 10:26:56 -0700

    Added second program

---
+++ MediaLib/MediaLib.java
@@ -0,0 +1,16 @@
+/**
+ * MediaLib descrip.
+ * 
+ * @author (Otakar Andrysek) 
+ * @version (.1)
+ * @date (9/12/2016)
+ */
+public class MediaLib
+{
+    public static void main()
+    {
+        System.out.println("Welcome to your Media Library");
+        Song song1 = new Song();
+        System.out.println(song1);
+    }
+}
---
+++ MediaLib/Song.java
@@ -0,0 +1,23 @@
+/**
+ * Song descrip.
+ * 
+ * @author (Otakar Andrysek) 
+ * @version (.1)
+ * @date (9/12/2016)
+ */
+public class Song
+{
+    // Instance variables - replace the example below with your own
+    private int rating;
+    private String title = "";
+
+    /**
+     * Constructor for objects of class Song
+     */
+    public Song()
+    {
+        // Initialise instance variables
+        rating = 0;
+        title = "";
+    }
+}
