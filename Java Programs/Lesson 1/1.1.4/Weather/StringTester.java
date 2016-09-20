cdce1abadc6919cea4ba0f306aba1e454e0848d7
Otakar Andrysek <otakar@MacBook-Pro-2.local>
2016-09-20 10:28:46 -0700

    Started 1.1.4

---
+++ Java Programs/Lesson 1/1.1.4/Weather/StringTester.java
@@ -0,0 +1,34 @@
+
+/**
+ * This is the main class in the Weather Project.
+ * 
+ * @author Otakar Andrysek
+ * @version .1
+ * @date 9/20/2016
+ */
+public class StringTester
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class StringTester
+     */
+    public StringTester()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
