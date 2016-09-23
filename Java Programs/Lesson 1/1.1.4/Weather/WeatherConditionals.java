a51069e76c2645ee138574b120c01339beacb56b
Otakar Andrysek <otakar@MacBook-Pro-2.local>
2016-09-22 23:28:57 -0700

    Nightly Update for 1.1.4

--- Java Programs/Lesson 1/1.1.4/Weather/WeatherConditionals.java
+++ Java Programs/Lesson 1/1.1.4/Weather/WeatherConditionals.java
@@ -11,7 +11,52 @@ public class WeatherConditionals
 {
     // Create a method to give weather advice based on temperature and weather
     public static String getWeatherAdvice(int temperature, String description){
+       boolean windy = false;
+       boolean sunny = false;
+       boolean snow = false;
+       boolean cold = false;
+       
+       // Logic to determine action based on weather conditions
+       if (temperature == 34 & description.contains("sunny"))
+       {
+           // Set booleans
+           sunny = true;
+           // Print the weather status
+           System.out.println("It's safe to go outside, 34 degrees and sunny.");
+       }
+       else if (temperature == 32 & description.contains("windy"))
+       {
+           windy = true;
+           // Print the weather status
+           System.out.println("Too windy or cold! Enjoy watching the weather through the window.");
+       }
+       else if (temperature == 33 & description.contains("snow"))
+       {
+           snow = true;
+           // Print the weather status
+           System.out.println("It's safe to go outside, 33 degrees and snow.");
+       }
+       else if (temperature == 30 & description.contains("snow"))
+       {
+           cold = true;
+           snow = true;
+           // Print the weather status
+           System.out.println("Too windy or cold! Enjoy watching the weather through the window.");
+       }
+       else if (temperature == 30 & description.contains("windy"))
+       {
+           cold = true;
+           windy = true;
+           // Print the weather status
+           System.out.println("Too windy or cold! Enjoy watching the weather through the window.");
+       }
+       else // Oh no, something went wrong!
+       {
+           // Print the (error) status
+           System.out.println("NO CONDITION FOUND [ERR 1]");
+       }
        // Return a combined string in format: "32 degrees and heavy snow."
-       return temperature + " degrees and " + description + "."; 
+       windy = description.contains("windy");
+       return temperature + " degrees and " + description + ". " + windy; 
     }
 }
