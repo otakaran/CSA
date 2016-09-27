5886cc49ddb9827cb62a357693522c9b27e52d84
Otakar Andrysek <otakar@MacBook-Pro-2.local>
2016-09-27 09:20:37 -0700

    1.1.5 (2)

--- Java Programs/Lesson 1/1.1.5/1.1.5SciFiName_Starter/SciFiName.java
+++ Java Programs/Lesson 1/1.1.5/1.1.5SciFiName_Starter/SciFiName.java
@@ -1,10 +1,10 @@
-
 /**
  * SciFiName gathers input from a user to generate a custom name that
  * sounds like it could be used in a science fiction book or movie.
  * 
- * @author ckinnard
- * @version 3/4/16
+ * @author Otakar Andrysek
+ * @version .1
+ * @date 9/26/2016
  */
 
 public class SciFiName   
@@ -15,13 +15,15 @@ public class SciFiName
      * 
      * For best results, user lowercase letters and do not use spaces in your input
      */
+    
     public static void main()
     {
+        // Print user welcome
         System.out.println("If you provide me some information I will provide a Science Fiction name for you.");
         System.out.println("Please have all responses  be at least three characters long.");
         System.out.println("For best results, user lowercase letters with no spaces.\n");
 
-        // notice this methd (print) does not print a newline
+        // Notice this method (print) does not print a newline
         System.out.print("Enter your first name: ");
         String firstName = UserInput.getString();
         System.out.print("Enter your last name: ");
@@ -34,9 +36,12 @@ public class SciFiName
         String relativeName1 = UserInput.getString();
         System.out.print("Enter the first name of a second sibling or relative: ");
         String relativeName2 = UserInput.getString();
+        
+        // Active variables: firstName, lastName, city, school, relativeName1, relativeName2
 
-        // generate a sciFi name
-   
+        // Generate a SciFi name
+        String SciFiName1 = firstName.substring(0,3) + lastName.substring(0,2);
+        String SciFiName2 = city.substring(0,2) + school.substring(0,3);
         
     }
 }
--- Java Programs/Lesson 1/1.1.5/1.1.5SciFiName_Starter/UserInput.java
+++ Java Programs/Lesson 1/1.1.5/1.1.5SciFiName_Starter/UserInput.java
@@ -1,14 +1,14 @@
+// Import a library that allows for user input, finally!
 import java.util.Scanner;
 
 /**
- * Convenience class for getting input from a user
+ * Convenience class for getting input from a user.
  */
+
 public class UserInput   
 {
     public static String getString() {
-        
-      Scanner in = new Scanner(System.in);
-      return in.next();
-    
+      Scanner in = new Scanner(System.in);  // Scans for user input
+      return in.next(); // Retuns input to caller function
    }
 }
