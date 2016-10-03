76d78dd3f987a1954dddd60fdabb461c824506b6
Otakar Andrysek <otakar@MacBook-Pro-2.local>
2016-10-02 21:54:16 -0700

    Finished 1.1.6

--- Java Programs/Lesson 1/1.1.6/MagpiePart2/Magpie2.java
+++ Java Programs/Lesson 1/1.1.6/MagpiePart2/Magpie2.java
@@ -1,36 +1,37 @@
 /**
  * A program to carry on conversations with a human user.
  * This is the initial version that:  
- * <ul><li>
  *       Uses indexOf to find strings
- * </li><li>
- *          Handles responding to simple words and phrases 
- * </li></ul>
+ *       Handles responding to simple words and phrases 
  * This version uses a nested if to handle default responses.
+ * 
+ * *ORIGINAL*
  * @author Laurie White
  * @version April 2012
+ * 
+ * *UPDATED*
+ * @author Otakar Andrysek
+ * @version 2.2
+ * @date 10/2/16
  */
+
 public class Magpie2
 {
-    /**
-     * Get a default greeting   
-     * @return a greeting
-     */
+    // Greet the user
     public String getGreeting()
     {
         return "Hello, let's talk.";
     }
     
-    /**
-     * Gives a response to a user statement
-     * 
-     * @param statement
-     *            the user statement
-     * @return a response based on the rules given
-     */
+    // Gives a response to a user statement
+    // @param statement - the user statement      
+    // @return a response based on the rules given
     public String getResponse(String statement)
     {
+        // Store the response
         String response = "";
+        
+        // Pick something based on keywords
         if (statement.indexOf("no") >= 0)
         {
             response = "Why not?";
@@ -41,7 +42,7 @@ public class Magpie2
         {
             response = "Tell me more about your family.";
         }
-        else if (statement.indexOf("sister") >= 0)
+        else if (statement.indexOf("sister") >= 0) 
         {
             response = "Sisters are always like that.";
         }
@@ -71,17 +72,17 @@ public class Magpie2
         return response;
     }
 
-    /**
-     * Pick a default response to use if nothing else fits.
-     * @return a non-committal string
-     */
+    // Pick a default response to use if nothing else fits.
+    // @return a non-committal string
     private String getRandomResponse()
     {
+        // Initialize local variables
         final int NUMBER_OF_RESPONSES = 6;
         double r = Math.random();
         int whichResponse = (int)(r * NUMBER_OF_RESPONSES);
         String response = "";
         
+        // Pick something from here based on the random number
         if (whichResponse == 0)
         {
             response = "Interesting, tell me more.";
@@ -107,4 +108,5 @@ public class Magpie2
             response = "Let's move on.";
         }
         return response;
-    }}
+    }
+}
