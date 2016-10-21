2d8bd520e3a783eeb2abf7dfe440131de7a42241
Otakar Andrysek <otakar@MacBook-Pro-2.local>
2016-10-21 15:54:00 -0700

    Part IV 1.2.1

--- Java Programs/Lesson 1/1.2/1.2.1/MediaLib 2/LoopingMediaLib.java
+++ Java Programs/Lesson 1/1.2/1.2.1/MediaLib 2/LoopingMediaLib.java
@@ -12,9 +12,16 @@ public class LoopingMediaLib
         while (songInfo != null)
         {
             songInfo = (MediaFile.readString());
-            if (songInfo != null)
+            if (songInfo != null)   // Needed to preven printing first 'null'
             {    
-                System.out.println(songInfo);
+                //System.out.println(songInfo);
+                int position = (songInfo.indexOf( '|' ));
+                String title = songInfo.substring(0, (position));
+                String rating = songInfo.substring(position + 1);
+                
+                String output = System.lineSeparator() + "Title: " + title + System.lineSeparator() 
+                                + "Rating: " + rating;
+                System.out.println(output);
             }
         }
         MediaFile.saveAndClose();
