8c3546125668c003f219b40682760777385761dd
Otakar Andrysek <otakar@MacBook-Pro-2.local>
2016-10-25 10:04:04 -0700

    Almost done with 1.2.2

--- Unit 1/1.2/1.2.2/Programs/Programs/MediaLib 2/ArrayMediaLib.java
+++ Unit 1/1.2/1.2.2/Programs/Programs/MediaLib 2/ArrayMediaLib.java
@@ -2,8 +2,9 @@
 /**
  * Write a description of class ArrayMediaLib here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Otakar Andrysek) 
+ * @version (1.0)
+ * @date (10/25/16)
  */
 public class ArrayMediaLib
 {
@@ -13,16 +14,31 @@ public class ArrayMediaLib
         String[ ] sharingFriends = {
             new String("Tamir"),
             new String("Dane"),
-            new String("Vikas"),
+            //new String("Vikas"),
             new String("Andrew"),
             new String("Stan") 
-        
         };
-        
+       
         // Iterate over my friends' array
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < sharingFriends.length; i++)     // i < length of array
         {
             System.out.println(sharingFriends[i]);  // Print elements in order
         }
+        
+        /**
+         * Part III
+        */
+       
+        // Days between purchase
+        int[ ] daysBetweenPurchase = {2, 5, 1, 2, 4, 2, 1, 3};
+        int sumDays = 0;
+        
+        // Calculate mean purchase time
+        for (int j = 0; j < daysBetweenPurchase.length; j++)     // i < length of array
+        {
+            sumDays += daysBetweenPurchase[j];
+        }
+        float meanDays = sumDays / daysBetweenPurchase.length;
+        System.out.println(meanDays);  // Print elements in order
     }
 }
